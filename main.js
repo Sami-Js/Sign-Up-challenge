48291cdf3d7a79a289a45b828259b7896f9a151f
Sami-Js <52254703+Sami-Js@users.noreply.github.com>
2019-10-25 00:45:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,16 +12,14 @@ class Fun {
     static addClass(val , nameClass) {
         // حفظ اب المدخل في متغير لكي تسهل اضافة الكلاس 
         let paranetInput = val.id.parentElement ;
+        paranetInput.className = 'inp';
         // حفظ الابن الذي ندخل فيه الرسالة
         let childMsg = paranetInput.children[2];
         paranetInput.classList.add(nameClass);
-        if(nameClass === 'failed')
-         childMsg.innerHTML = val.msg ;
-         else
-         childMsg.innerHTML = '';
+        nameClass === 'failed' ? childMsg.innerHTML = val.msg : childMsg.innerHTML = '';
     }
 
-    // دالة اعادة تعيين القيمة  للحقول والكلاس للاب
+    // دالة اعادة تعيين القيمة للمدخلات وارجاع كلاس الأب للافتراضي لكي يحذف الكلاسات المضافه 
     static removeValue(arrValues) {
         arrValues.forEach((e) => {
         let paranetInput = e.id.parentElement ;
@@ -34,6 +32,7 @@ class Fun {
 
 // الضغط والتأكد من القيم
 function generateValue() {
+
     const name = new Inputs('name', 'Please enter a valid name');
     const email = new Inputs('email', 'Please enter a valid email Address');
     const password = new Inputs('password', 'Please enter a valid Password');
@@ -46,12 +45,9 @@ function generateValue() {
       return ;
      }
 
-
+     // شرط التحقق من قيم المدخلات
     ArrayInputs.forEach((element) => {
-    if(!element.id.value)
-    Fun.addClass( element , 'failed');
-    else
-    Fun.addClass( element , 'success');
+     !element.id.value ? Fun.addClass( element , 'failed'):Fun.addClass( element , 'success')
     });
 
 }
